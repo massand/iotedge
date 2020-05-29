052f04969496eefb6748fc2901a3926efb6874be
Rajeev Massand <massand@gmail.com>
2020-05-28 20:12:12 -0700

    Identity Service and Key Service launch

---
+++ edgelet/edgelet-http-identity/src/identity.rs
@@ -0,0 +1,51 @@
+// Copyright (c) Microsoft. All rights reserved.
+
+use crate::error::{Error, ErrorKind};
+use edgelet_core::{AuthId, WorkloadConfig};
+use edgelet_http::route::{Handler, Parameters};
+use edgelet_http::Error as HttpError;
+use failure::ResultExt;
+use futures::{future, Future};
+use hyper::header::{CONTENT_LENGTH, CONTENT_TYPE};
+use hyper::{Body, Request, Response, StatusCode};
+
+pub struct IdentityHandler<W: WorkloadConfig> {
+    config: W,
+}
+
+impl<W: WorkloadConfig> IdentityHandler<W> {
+    pub fn new(config: W) -> Self {
+        IdentityHandler { config }
+    }
+}
+
+impl<W> Handler<Parameters> for IdentityHandler<W>
+where
+    W: WorkloadConfig + Clone + Send + Sync + 'static,
+{
+    fn handle(
+        &self,
+        req: Request<Body>,
+        params: Parameters,
+    ) -> Box<dyn Future<Item = Response<Body>, Error = HttpError> + Send> {
+        let config = self.config.clone();
+
+        let body = req
+            .extensions()
+            .get::<AuthId>()
+            .map_or_else(
+                || "AuthId expected".to_string(),
+                |auth_id| format!("auth = {}", auth_id),
+            )
+            .into();
+
+        let response = Response::builder()
+            .status(StatusCode::OK)
+            .header(CONTENT_TYPE, "application/json")
+            .header(CONTENT_LENGTH, "10")
+            .body(body)
+            .unwrap();
+
+        Box::new(future::ok(response))
+    }
+}
---
+++ edgelet/edgelet-http-keyservice/src/error.rs
@@ -0,0 +1,51 @@
+// Copyright (c) Microsoft. All rights reserved.
+
+use failure::{Backtrace, Context, Fail};
+use std::fmt::{self, Display};
+
+#[derive(Debug)]
+pub struct Error {
+    inner: Context<ErrorKind>,
+}
+
+#[derive(Clone, Debug, Fail)]
+pub enum ErrorKind {
+    #[fail(display = "Could not start key service")]
+    StartService,
+}
+
+impl Fail for Error {
+    fn cause(&self) -> Option<&dyn Fail> {
+        self.inner.cause()
+    }
+
+    fn backtrace(&self) -> Option<&Backtrace> {
+        self.inner.backtrace()
+    }
+}
+
+impl Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        Display::fmt(&self.inner, f)
+    }
+}
+
+impl Error {
+    pub fn kind(&self) -> &ErrorKind {
+        self.inner.get_context()
+    }
+}
+
+impl From<ErrorKind> for Error {
+    fn from(kind: ErrorKind) -> Self {
+        Error {
+            inner: Context::new(kind),
+        }
+    }
+}
+
+impl From<Context<ErrorKind>> for Error {
+    fn from(inner: Context<ErrorKind>) -> Self {
+        Error { inner }
+    }
+}
